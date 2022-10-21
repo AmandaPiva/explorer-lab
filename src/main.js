02b7711f57173f36cff9a90fda451998039f69ef
Amanda Piva <amandamariapiva@gmail.com>
2022-10-21 18:30:15 -0300

    Adicionando masks nos inputs, e usamos o regex para modificar tipo do cartão

--- src/main.js
+++ src/main.js
@@ -1,19 +1,50 @@
 import "./css/index.css"
+import IMask from "imask"
 
-const CardColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
-const CardColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
-const getLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+const CardColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const CardColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const getLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-function setCardType(type){
+function setCardType(type) {
   const colors = {
     visa: ["#2D57F2", "#436D99"],
     mastercard: ["#DF6F29", "#C69347"],
-    default: ["black", "gray"]
+    default: ["black", "gray"],
   }
 
-  CardColor1.setAttribute("fill", colors[type][0]);
-  CardColor2.setAttribute("fill", colors[type][1]);
-  getLogo.setAttribute("src", `cc-${type}.svg`);
+  CardColor1.setAttribute("fill", colors[type][0])
+  CardColor2.setAttribute("fill", colors[type][1])
+  getLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCardType('default');
+setCardType("default")
+
+//Security-code
+const securityCode = document.querySelector("#security-code")
+
+const securityCoderPathern = {
+  mask: "0000",
+}
+
+const securityCodeMasked = IMask(securityCode, securityCoderPathern)
+
+//Expiration date
+const expirationDate = document.querySelector("#expiration-date")
+
+const expirationDatePathern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePathern)
